4d2a3012bf6313d0bf02216ebb19ac2c98dcd118
SaulChasipanta10 <saulchasipanta@gmail.com>
2024-11-02 09:44:51 -0500

    Add files via upload

---
+++ Archivo prueba.java
@@ -0,0 +1,9 @@
+Class HolaPOO{
+// Este es un comentario de una sola línea en Java
+      public static void main(String[] args){
+// NO SE PROGRAMAR :(
+      System.out.println("Hola curso de la ESPE y profe :");
+// Para un 20 el deber
+}
+
+}
