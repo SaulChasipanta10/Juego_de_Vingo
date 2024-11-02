07ac7417f630681196c3e8f69d7b7dff1ec93169
SaulChasipanta10 <saulchasipanta@gmail.com>
2024-11-02 09:37:28 -0500

    Update Archivo prueba.java

--- Archivo prueba.java
+++ Archivo prueba.java
@@ -2,7 +2,7 @@ Class HolaPOO{
 // Este es un comentario de una sola línea en Java
       public static void main(String[] args){
 
-      System.out.println("Hola curso de POO");
+      System.out.println("Hola curso de la ESPE y profe :)");
 // Para un 20 el deber
 }
 
